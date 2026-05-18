8325b92617f000fe165ac91750221cad956c0fbb
nguyendinhson0154 <nguyendinhson0154@gmail.com>
2026-05-18 18:41:27 +0700

    chore: convert server, admin, and client into submodules

--- solar_tpc_admin/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './TsoApp.vue'
-import router from './router/index'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
